6bfb340f12d089c6a85844f9d50c75f56110c555
206 <206@academicos.cesde.edu.co>
2022-05-18 09:58:41 -0500

    se crean token con post

--- servicio.js
+++ servicio.js
@@ -4,7 +4,7 @@ const URI ="https://api.spotify.com/v1/artists/716NhGYqD1jl2wI1Qkgq36/top-tracks
 
 //2. DECLARO LOS PARAMETROS DE LA PETICION (QUE VOY A HACER)
 
-const TOKEN = "Bearer BQCYb6IRliFg8UQzAZpbf3XvARQuCqJYzf4a-3aJ-316jnIHYxRuPHkXEnW7XVv4LXS9zPeOg237RoYgdKipc6aME0Q6yeibi_ZWsqfMehr_oeOs7-aAp-jcFSO4TmuB1xxwAiii6QDPziFspsuInRkgNzyD624YhnI"
+const TOKEN = "Bearer BQDIS9iCighY-GkF_K7HHNDe8rUVVKWPTuxyL4DG4MP3TMQ9oEPwWBqLhON8ho7AaAY6bVKDgKT8IPH4D3JsG1E2vDMobYUdSXYgLrPnWJBh6nqGZCku3kWtb1qfF8l16nb60UMNJ-6QxS5DQykDUPsb11CDtQpyizY"
 
 const PETICION ={
 
@@ -12,7 +12,76 @@ method:"GET",
 headers: {Authorization:TOKEN}
 
 }
-// DECLARO EL FETCH(VOY AL SERVIDOR)
+
+
+//Rutina para consumir api con metodo post
+// 1. Uri para donde voy
+
+const URIPOST = 'https://accounts.spotify.com/api/token'
+
+//2. almaceno los datos que voy a enviarr
+
+let dato1  ='client_id=4e183525652b4b60891a0c3c79f073f3'
+
+let dato2 ='client_secret=1f80fd4d69ca4441ad0cb3c04a8efa3e'
+
+let dato3 = 'grant_type=client_credentials'
+
+
+//3. Configuro la peticion
+
+const PETICIONPOST={
+
+method: "POST",
+headers:{
+
+"Content-type":"application/x-www-form-urlencoded"
+
+},
+body:dato1+'&'+dato2+'&'+dato3
+
+}
+
+
+
+//4. VOY AL SERVIDOR A CONSUMIR EL SERVICIO
+ fetch(URIPOST,PETICIONPOST)
+.then(function(respuesta){
+    return respuesta.json()
+})
+.then(function(respuesta){
+    console.log(respuesta)
+    let token = respuesta.token_type+' '+respuesta.access_token
+    console.log(token)
+})
+.catch(function(respuesta){
+console.log(respuesta)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*DECLARO EL FETCH(VOY AL SERVIDOR)
 fetch(URI,PETICION)
 .then(function(respuesta){
 
@@ -22,10 +91,62 @@ return (respuesta.json())
 .then(function(respuesta){
 
     console.log(respuesta)//respuesta es un objeto
-    console.log(respuesta.tracks)
-
-    console.log(respuesta.tracks[1].preview_url)
+    console.log(respuesta.tracks)//arreglo de 10 canciones
+pintarCanciones(respuesta.tracks)
+    
 })
-.catch(function(){
+.catch(function(respuesta){
     console.log(respuesta)
 })
+ 
+///FUNCION PARA RECORRER UN ARREGLO
+
+function pintarCanciones(canciones){
+
+let fila = document.getElementById("fila")
+
+
+
+
+
+
+canciones.forEach(function(cancion){
+console.log(cancion)
+console.log(cancion.name)
+console.log(cancion.preview_url)
+console.log(cancion.album.images[0].url)
+
+
+
+//RUTINA PARA LLENADO
+ let columna = document.createElement("div")
+ columna.classList.add("col")
+
+ let tarjeta =document.createElement("div")
+ tarjeta.classList.add("card","h-100","shadow")
+
+
+ let audio= document.createElement("audio")
+ audio.classList.add("w-100")
+audio.setAttribute("controls","controls")
+audio.src = cancion.preview_url
+
+let foto = document.createElement("img")
+foto.classList.add("img-fluid","w-100")
+foto.src = cancion.album.images[0].url
+
+//padres e hijos
+tarjeta.appendChild(foto)                                                 
+tarjeta.appendChild(audio)
+columna.appendChild(tarjeta)
+fila.appendChild(columna)
+ 
+
+
+
+
+})
+
+
+
+}*/
